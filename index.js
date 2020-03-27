9e2e10bdcab55f210f11b35dc4e2d158e4618850
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-03-27 21:53:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const token = '';
 
 //Check if the bot is online/ready.
 bot.on('ready', () => {
-    console.log("Insider Is Ready");
+    console.log("Bot Is Ready");
 })
 
+//if the user say hello the bot would reply
 bot.on('message', msg=> {
     if(msg.content === "Hello") {
         msg.reply('Hello I Am InsiderBot.My Creator Is InsiderJanggo');
